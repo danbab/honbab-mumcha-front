7efabad4c2bef9eff090e9cf9a2cb232105aa044
thkim610 <thkim610@naver.com>
2023-11-02 21:39:19 +0900

    feat : Radio 컴포넌트 추가 #4

---
+++ src/components/Radio.js
@@ -0,0 +1,19 @@
+
+const Radio = ({ children, value, name, ...restProps }) => {
+
+
+
+    return (
+      <label className="mx-[2.25rem]">
+        <input
+          type="radio"
+          value={value}
+          name={name}
+          className="mr-2 mt-2 "
+        />
+        {children}
+      </label>
+    );
+  }
+
+export default Radio;
