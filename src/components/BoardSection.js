7f9dc7091569a308da2471f9be5f87387c71f427
danbab <danbab2@naver.com>
2023-11-03 14:48:32 +0900

    style : 게시물 렌더링 1차 완성 #3

--- src/components/BoardSection.js
+++ src/components/BoardSection.js
@@ -3,18 +3,16 @@ import Input from "./Input";
 
 const BoardSection = ({ children }) => {
   return (
-    <div className="p-4 sm:ml-64 bg-slate-400">
-      <div className="flex flex-wrap justify-between items-center ">
+    <div className="p-4 sm:ml-64 bg-slate-400 flex-grow">
+      <div className="flex mb-5 ml-[4.7rem] flex-wrap justify-between items-center ">
         <Button type="make-reservation">지금 약속잡기</Button>
-        <div className="flex">
+        <div className="flex mr-[4.7rem]">
           <Input type="board-search-input" name="검색창" />
           <Button type="search">검색</Button>
         </div>
       </div>
-
-      <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
-        <div className="grid grid-cols-4 gap-4 mb-4">{children}</div>
-      </div>
+      {/* <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700"> */}
+      <div className="grid grid-cols-4 gap-4 mb-4 ml-[4.7rem]">{children}</div>
     </div>
   );
 };
