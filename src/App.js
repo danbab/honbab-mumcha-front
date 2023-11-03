9959ae40dcf3d44a26356e6274066c5dc7deeaf2
danbab <danbab2@naver.com>
2023-11-03 14:55:31 +0900

    Mod : import main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import MainPage from "./pages/MainPage";
+import MainPage from "./pages/MainPage";
 //import LoginPage from "pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import BoardPage from "./pages/BoardPage";
