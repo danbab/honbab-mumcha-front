7e37fae997c4f86d59231f7f3749805ed0e7622d
RSH <fbtnghks7906@gmail.com>
2023-11-03 21:12:19 +0900

    feat: 게시글 상세 페이지에 제목 및 내용 컴포넌트 추가 #12

--- src/pages/BoardDetailPage.js
+++ src/pages/BoardDetailPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import DetailPageHeader from "../components/DetailPageHeader";
+import DetailPageTitleAndContentSection from "../components/DetailPageTitleAndContentSection";
 
 const BoardDetailPage = () => {
   return (
     <>
       <DetailPageHeader />
+      <DetailPageTitleAndContentSection />
     </>
   );
 };
