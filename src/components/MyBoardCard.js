3a1b74348597392cdf2743f3988990e42b9f2cc9
wan-ding <weew9900@gmail.com>
2023-11-08 19:52:58 +0900

    [feat] 백에서 가져오는 정보로 메인 페이지에 게시글 보여주기

---
+++ src/components/MyBoardCard.js
@@ -0,0 +1,41 @@
+const MyBoardCard = ({ myBoards }) => {
+  let style = "rounded-[0.625rem] shadow-md";
+
+  return (
+    <div className="bg-[#FDFDFD] w-[15.625rem] h-[24.875rem] rounded-[1.25rem] relative">
+      <div className="w-[14.25rem] h-[21.875rem] flex justify-between mx-auto">
+        <div className="flex gap-2 ml-2 mt-4">
+          <img className="w-[2.125rem] h-[2.125rem]" src="img/iamlogo.svg" />
+          {/* 작성자 이름 */}
+          <div className="text-[#000] text-[12px] mt-3">
+            {myBoards.writer.name}
+          </div>
+        </div>
+        {/* 작성일-reg_date */}
+        <div className="text-[#BFBFBF] text-[10px] mt-8 mr-[1.5rem]">
+          {myBoards.reg_date}
+        </div>
+      </div>
+      <img
+        className="absolute top-[4.5rem] right-0 w-full h-[12.1875rem] "
+        src="img/boardexampleimg.svg"
+      />
+      {/* 글 제목-title */}
+      <p className="absolute bottom-[6rem] left-[1rem] text-[0.9375rem] font-bold">
+        {myBoards.title}
+      </p>
+      <ul className="list-disc absolute bottom-[3.8rem] left-[2rem] ">
+        <li className="text-[#8B8686] text-[0.5rem]">태그1</li>
+        <li className="text-[#8B8686] text-[0.5rem]">태그2</li>
+      </ul>
+      <button
+        className="rounded-[0.625rem] text-[0.5rem] w-[3rem] h-[1.50rem] bg-[#54AB75] text-[#ffffff] absolute bottom-[1rem] left-[1rem] shadow-md"
+        type="button"
+      >
+        참여하기
+      </button>
+    </div>
+  );
+};
+
+export default MyBoardCard;
