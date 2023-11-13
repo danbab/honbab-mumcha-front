712bc0fd824124d9b460e95e4b363ffd7bc59b5d
RSH <fbtnghks7906@gmail.com>
2023-11-13 22:14:36 +0900

    fix: 게시글 카드 전체에 링크 -> 제목에만 링크 줌

--- src/components/BoardCard.js
+++ src/components/BoardCard.js
@@ -22,72 +22,72 @@ const Card = ({ boardDto }) => {
     .replace(/ /g, "");
 
   return (
-    <Link to={`/boardDetail`} state={{ id: boardDto.boardId }}>
-      <div className="bg-[#FDFDFD] w-[15.625rem] h-[24.875rem] rounded-[1.25rem] relative">
-        <div className="w-[14.25rem] h-[21.875rem] flex justify-between mx-auto">
-          <div className="flex gap-2 ml-2 mt-[1.25rem]">
-            <img className="w-[2.125rem] h-[2.125rem]" src="img/iamlogo.svg" />
-            {/* 작성자 이름 */}
-            <div className="text-[#000] text-[12px] mt-3">
-              {boardDto.writer.name}
-            </div>
+    <div className="bg-[#FDFDFD] w-[15.625rem] h-[24.875rem] rounded-[1.25rem] relative">
+      <div className="w-[14.25rem] h-[21.875rem] flex justify-between mx-auto">
+        <div className="flex gap-2 ml-2 mt-[1.25rem]">
+          <img className="w-[2.125rem] h-[2.125rem]" src="img/iamlogo.svg" />
+          {/* 작성자 이름 */}
+          <div className="text-[#000] text-[12px] mt-3">
+            {boardDto.writer.name}
           </div>
-          {/* 작성일-reg_date */}
-          <div className="text-[#BFBFBF] text-[10px] mt-[2.1rem] mr-[0.5rem]">
-            {date}
-          </div>
-          {boardDto.regDate != boardDto.lastModified && (
-            <span className="absolute top-12 right-[1.2rem] text-[#BFBFBF] text-[10px]">
-              수정됨
-            </span>
-          )}
         </div>
-        <img
-          className="absolute top-[4.5rem] right-0 w-full h-[12.1875rem] "
-          src="img/boardexampleimg.svg"
-        />
+        {/* 작성일-reg_date */}
+        <div className="text-[#BFBFBF] text-[10px] mt-[2.1rem] mr-[0.5rem]">
+          {date}
+        </div>
+        {boardDto.regDate != boardDto.lastModified && (
+          <span className="absolute top-12 right-[1.2rem] text-[#BFBFBF] text-[10px]">
+            수정됨
+          </span>
+        )}
+      </div>
+      <img
+        className="absolute top-[4.5rem] right-0 w-full h-[12.1875rem] "
+        src="img/boardexampleimg.svg"
+      />
 
+      <Link to={`/boardDetail`} state={{ id: boardDto.boardId }}>
         {/* 글 제목-title */}
         <p className="absolute bottom-[6rem] left-[1rem] ml-1 text-[1rem] font-bold">
           {boardDto.title}
         </p>
-        <Heart
-          className="w-[1.5rem] absolute bottom-[6rem] right-[1.1rem]"
-          isActive={active}
-          onClick={() => setActive(!active)}
-          animationTrigger="both"
-          inactiveColor="rgba(255,125,125,.75)"
-          activeColor="#E14949"
-          animationDuration={0.1}
-        />
+      </Link>
+      <Heart
+        className="w-[1.5rem] absolute bottom-[6rem] right-[1.1rem]"
+        isActive={active}
+        onClick={() => setActive(!active)}
+        animationTrigger="both"
+        inactiveColor="rgba(255,125,125,.75)"
+        activeColor="#E14949"
+        animationDuration={0.1}
+      />
 
-        <ul className="list-disc absolute bottom-[3.8rem] left-[2rem] ">
-          <li className="text-[#8B8686] text-[0.5rem]">태그1</li>
-          <li className="text-[#8B8686] text-[0.5rem]">태그2</li>
-        </ul>
+      <ul className="list-disc absolute bottom-[3.8rem] left-[2rem] ">
+        <li className="text-[#8B8686] text-[0.5rem]">태그1</li>
+        <li className="text-[#8B8686] text-[0.5rem]">태그2</li>
+      </ul>
 
-        <div className="absolute bottom-[1rem] left-[1rem]">
-          <div className="w-[14.25rem] h-[2rem] flex justify-between ">
-            <button
-              className="rounded-[0.625rem] text-[0.8rem] w-[4rem] h-[1.85rem] bg-[#54AB75] text-[#ffffff]  shadow-md"
-              type="button"
-            >
-              참여하기
-            </button>
-            {/* 상태에 따라 모집중 또는 모집완료 표시 */}
-            {boardDto.status === 1 ? (
-              <span className="mt-1 mr-4 text-[0.9rem] text-green-500 font-bold">
-                모집중
-              </span>
-            ) : (
-              <span className="mt-1 mr-4 text-[0.9rem] text-red-500 font-bold">
-                모집완료
-              </span>
-            )}
-          </div>
+      <div className="absolute bottom-[1rem] left-[1rem]">
+        <div className="w-[14.25rem] h-[2rem] flex justify-between ">
+          <button
+            className="rounded-[0.625rem] text-[0.8rem] w-[4rem] h-[1.85rem] bg-[#54AB75] text-[#ffffff]  shadow-md"
+            type="button"
+          >
+            참여하기
+          </button>
+          {/* 상태에 따라 모집중 또는 모집완료 표시 */}
+          {boardDto.status === 1 ? (
+            <span className="mt-1 mr-4 text-[0.9rem] text-green-500 font-bold">
+              모집중
+            </span>
+          ) : (
+            <span className="mt-1 mr-4 text-[0.9rem] text-red-500 font-bold">
+              모집완료
+            </span>
+          )}
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
