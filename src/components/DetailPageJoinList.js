49a215883a72891e075b55689e8584840c7e2cd6
RSH <fbtnghks7906@gmail.com>
2023-11-21 02:03:57 +0900

    feat: 상세페이지에 참여자 리스트 렌더링

--- src/components/DetailPageJoinList.js
+++ src/components/DetailPageJoinList.js
@@ -1,37 +1,70 @@
-import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useState, useEffect } from "react";
+import { useCookies } from "react-cookie";
+import LoadingSpinner from "../components/LoadingSpinner";
+import DetailPageParticipantsCard from "../components/DetailPageParticipantsCard";
 
-const DetailPageJoinList = () => {
+const DetailPageJoinList = ({ boardData, isLoading }) => {
   const [participationData, setParticipationData] = useState([]);
+  const [cookies] = useCookies();
+  const token = cookies.token;
 
-  // API에서 데이터를 가져오는 함수
-  const fetchParticipationData = async () => {
-    try {
-      const response = await axios.get('http://localhost:8080/api/my/');
-      setParticipationData(response.data);
-    } catch (error) {
-      console.error("Error fetching data: ", error);
+  console.log("조인리스트에서 쓸: " + JSON.stringify(boardData));
+
+  const requestOption = {
+    headers: {
+      Authorization: `Bearer ${token}`
     }
   };
 
   useEffect(() => {
+    const fetchParticipationData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/api/my/party/${boardData.boardId}`, requestOption);
+        setParticipationData(response.data);
+        console.log('axios로 값 받아온 데이터:', participationData);
+      } catch (e) {
+        console.error("데이터 가져오기 실패!: ", e);
+      }
+    };
     fetchParticipationData();
   }, []);
 
+  console.log("참여자 정보요: " + JSON.stringify(participationData));
+
+  if (isLoading || !boardData || !participationData) {
+    return <LoadingSpinner />;    //로딩 중일 때는 LoadingSpinner 컴포넌트를 렌더링
+  }
+
   return (
-    <div className='felx justify-between boader-2'>
-      <h3>참여 현황</h3>
+    <div className="grid grid-cols-2 gap-4 text-center justify-between max-w-screen-sm mx-auto my-10 rounded-[2rem] border-2 border-gray-200 bg-white">
+      <p className="col-span-2 mt-2 text-xl text-center font-sans ">참가 현황</p>
       {participationData.length > 0 ? (
-        <ul>
-          {participationData.map((participant, index) => (
-            <li key={index}>
-              <p>{participant.name}</p>
-              <p>{participant.status}</p>
-            </li>
-          ))}
-        </ul>
+        <div className='col-span-2'>
+          <ul className='grid grid-cols-2 gap-4 justify-items-center items-center'>
+            {participationData.map((participant, index) => {
+              // 참가자 생년월일 -> 나이로 변환해주는 로직
+              const birthDate = new Date(participant.birth);
+              const today = new Date();
+              let age = today.getFullYear() - birthDate.getFullYear();
+              let m = today.getMonth() - birthDate.getMonth();
+              if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+                age--;
+              }
+
+              return (
+                <DetailPageParticipantsCard
+                  key={index}
+                  isLoading={isLoading}
+                  participants={participant}
+                  age={age}
+                />
+              );
+            })}
+          </ul>
+        </div>
       ) : (
-        <p>참여자 정보가 없습니다.</p>
+        <p className="grid-cols-2">참가자 정보가 없습니다.</p>
       )}
     </div>
   );
