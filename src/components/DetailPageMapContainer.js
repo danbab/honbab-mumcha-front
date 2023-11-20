49829dbe4c456b8589644c66cc0362a97e60a8ad
RSH <fbtnghks7906@gmail.com>
2023-11-21 02:04:29 +0900

    style: 맵 마커 인포 스타일 수정

--- src/components/DetailPageMapContainer.js
+++ src/components/DetailPageMapContainer.js
@@ -18,9 +18,9 @@ const DetailPageMapContainer = ({ boardData }) => {
     };
     const map = new kakao.maps.Map(container, options);
 
-    let imageSrc = "img/locationicon.png", // 마커이미지의 주소입니다    
+    let imageSrc = "/img/locationicon.png", // 마커이미지의 주소입니다    
       imageSize = new kakao.maps.Size(80, 80), // 마커이미지의 크기입니다
-      imageOption = { offset: new kakao.maps.Point(30, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+      imageOption = { offset: new kakao.maps.Point(40, 60) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
     // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
     let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
@@ -35,10 +35,9 @@ const DetailPageMapContainer = ({ boardData }) => {
       kakao.maps.event.addListener(marker, "click", function () {
         //마커를 클릭하면 장소명이 인포윈도우에 표시
         infowindow.setContent(
-          '<div style="display: flex; justify-content: center; align-items: center; padding:0px; font-size:20px;">' +
+          `<div style="display: flex; position: relative; text-align: center; vertical-align: middle; font-size: 18px;">` +
           boardData.restaurantName +
-          "</div>"
-
+          `</div>`
         );
         infowindow.open(map, marker);
       });
@@ -53,7 +52,7 @@ const DetailPageMapContainer = ({ boardData }) => {
       <div>
         <div
           id="myMap"
-          className="w-[78.75rem] xl:w-[78rem] h-[45.125rem] overflow-hidden"
+          className="w-full h-[40rem] overflow-hidden"
         ></div>
       </div>
     </>
