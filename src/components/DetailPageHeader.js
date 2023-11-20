ff80e9125ea915e67ae2926f5eb0fc769fb38f84
RSH <fbtnghks7906@gmail.com>
2023-11-21 02:03:02 +0900

    fix: 게시글 작성시간 함수 변경, 로딩 컴포넌트 조건 변경

--- src/components/DetailPageHeader.js
+++ src/components/DetailPageHeader.js
@@ -1,74 +1,74 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "../components/Button";
-import Heart from "react-heart"
+import Heart from "../components/Heart"
 import LoadingSpinner from "../components/LoadingSpinner";
 /*
  * 게시글 상세 페이지 헤더
  * 구성 요소: 유저 아이콘, 유저 닉네임, 게시글 작성시간, 참여하기 버튼, 좋아요 버튼
  */
 
-//작성시간을 N시간 전으로 변환해주는 함수
-function timeDifference(current, previous) {
-  const msPerMinute = 60 * 1000;
-  const msPerHour = msPerMinute * 60;
-  const msPerDay = msPerHour * 24;
-  const msPerMonth = msPerDay * 30;
-  const msPerYear = msPerDay * 365;
+const DetailPageHeader = ({ boardData, isLoading, userId }) => {
+  // 게시글 작성시간 N시간 전으로 변경하는 함수
+  const [regDate, setRegDate] = useState("");
 
-  const elapsed = current - previous;
+  useEffect(() => {
+    const date = new Date(boardData.regDate);
+    const now = new Date();
+    const diff = now - date;
+    const hours = Math.floor(diff / (1000 * 60 * 60));
+    if (hours >= 24) {
+      setRegDate(`${Math.ceil(hours / 24)}일 전`);
+    } else {
+      setRegDate(`${hours}시간 전`);
+    }
+  }, [boardData.regDate]);
 
-  if (elapsed < msPerMinute) {
-    return Math.round(elapsed / 1000) + '초 전';
-  }
-  else if (elapsed < msPerHour) {
-    return Math.round(elapsed / msPerMinute) + '분 전';
-  }
-  else if (elapsed < msPerDay) {
-    return Math.round(elapsed / msPerHour) + '시간 전';
-  }
-}
-
-const DetailPageHeader = ({ boardData, isLoading }) => {
   // 좋아요 버튼 active/inactive
-  console.log('이건 props로 준 보드 데이터:' + boardData);
   const [active, setActive] = useState(false);
 
-
-  if (isLoading) {
-    return <LoadingSpinner />;  //로딩 중일 때는 LoadingSpinner 컴포넌트를 렌더링
+  if (isLoading || !boardData) {
+    return <LoadingSpinner />;    //로딩 중일 때는 LoadingSpinner 컴포넌트를 렌더링
   }
 
   return (
-    <div className="flex justify-between items-center max-w-screen-md md:mx-auto md:p-16 md:mt-10">
+    <div className="flex justify-between items-center max-w-screen-md md:mx-auto md:p-16 md:mt-10 font-sans">
       <div className="flex items-center space-x-2">
-        <img src="img/usericon.svg" alt="유저아이콘" className="w-12 h-12 rounded-full" />
+        <img src="/img/usericon.svg" alt="유저아이콘" className="w-12 h-12 rounded-full" />
         <div>
-          <div className="text-black text-base font-bold font-['Inter']">
+          <div className="text-gray-950 text-base font-bold">
             {/* 모집글 작성자 */}
             <p>{boardData.writer.name}</p>
           </div>
 
-          <div className="text-neutral-400 text-base font-extralight font-['Inter']">
+          <div className="text-neutral-400 text-base font-extralight">
             {/* 모집글 작성일시 */}
-            <p>{timeDifference(new Date(), new Date(boardData.regDate))}</p>
+            <p>{regDate}</p>
           </div>
         </div>
       </div>
 
       <div className="flex items-center space-x-5">
         {/* 조회수 */}
-        <div className="text-neutral-400 text-lg font-extralight font-['Inter']">
+        <div className="text-neutral-500 text-lg font-extralight">
           <p>조회수: {boardData.hit}</p>
         </div>
 
-        <p><Button type="big-join" >참여하기</Button></p>
-        <div className="inline-block">
+        <div className="inline-flex space-x-5">
+          <p><Button type="big-join" >참여하기</Button></p>
           <p>
-            <Heart className="w-[2.5rem]" isActive={active} onClick={() => setActive(!active)}
+            <Heart className="w-[2.4rem]" isActive={active} onClick={() => setActive(!active)}
               animationTrigger="both" inactiveColor="rgba(255,125,125,.75)"
               activeColor="#E14949" animationDuration={0.1} />
           </p>
         </div>
+
+        {/* 수정 및 삭제 버튼 추가 */}
+        {userId === boardData.writer.id && (
+          <div className="flex space-x-2">
+            {/* <p><Button type="board-modify">수정</Button></p> */}
+            <p><Button type="board-delete">삭제</Button></p>
+          </div>
+        )}
       </div>
     </div>
   );
