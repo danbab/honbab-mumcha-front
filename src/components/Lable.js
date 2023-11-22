e31be5b020b0f7427fc756159c00f2df4e26ce23
rowoon <dev.rowoon@gmail.com>
2023-11-22 14:20:17 +0900

    style: 마이페이지 수정

--- src/components/Lable.js
+++ src/components/Lable.js
@@ -1,21 +1,20 @@
 const Lable = ({ children, type = "lable", name = "", ...restProps }) => {
-    let style = "block text-sm font-medium leading-6";
-  
-    switch (type) {
-      case "register-lable":
-        style += " text-gray-900 w-[5rem] text-right mt-[0.3rem]";
-        break;
-  
-      
-      default:
-        break;
-    }
-  
-    return (
-      <label type={type} className={style} {...restProps}>
-        {children}
-      </label>
-    );
-  };
-  
-  export default Lable;
+  let style = "text-sm font-medium leading-6";
+
+  switch (type) {
+    case "register-lable":
+      style += " text-gray-900 w-[5rem] text-right mt-[0.3rem]";
+      break;
+
+    default:
+      break;
+  }
+
+  return (
+    <lable type={type} className={style} {...restProps}>
+      {children}
+    </lable>
+  );
+};
+
+export default Lable;
--- src/components/MyPageSection.js
+++ src/components/MyPageSection.js
@@ -3,22 +3,15 @@ import Button from "./Button";
 import Input from "./Input";
 
 const BoardSection = ({ children, fetchBoardDataByKeyword }) => {
-  const [searchKeyword, setSearchKeyword] =useState();
+  const [searchKeyword, setSearchKeyword] = useState();
 
   const handleSearch = () => {
     fetchBoardDataByKeyword(searchKeyword);
-  }
+  };
   return (
     <div className="p-4 sm:ml-64 bg-[#F6F6F6] flex-grow">
       <div className="flex mb-5 ml-[4.7rem] flex-wrap justify-between items-center ">
-        <div className="flex mr-[6.85rem]">
-          <Input 
-          type="board-search-input" 
-          name="검색창"
-          onChange={(e)=>setSearchKeyword(e.target.value)}
-           />
-          <Button type="search" onClick={handleSearch}>검색</Button>
-        </div>
+        <div className="flex mr-[6.85rem]"></div>
       </div>
       {/* <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700"> */}
       <div className="grid grid-cols-5 gap-4 mb-4 mx-[4.7rem]">{children}</div>
