64162400689d38b46f7e6002cb05b9b219affb56
rowoon <dev.rowoon@gmail.com>
2023-11-22 14:00:54 +0900

    style: 지도 오버레이 변경

--- src/components/CustomChart.js
+++ src/components/CustomChart.js
@@ -2,30 +2,30 @@ import { Link } from "react-router-dom";
 
 const CustomChart = ({ board }) => {
   return (
-    <div className="bg-[#f1f1f1] w-[14.42rem] h-[7.3rem] rounded-[1rem] pt-2 pl-2">
+    <div className="bg-[#2a2a2a] w-[14.42rem] h-[7.3rem] rounded-[1rem] pt-2 pl-2">
       <div className="flex gap-3">
         <div className="flex flex-col">
           <div className="bottom-[7.5rem] left-[0.3rem] w-[5.725rem]">
-            <p className="text-[0.8rem] overflow-ellipsis overflow-hidden whitespace-nowrap">
+            <p className="text-[0.8rem] text-[#ffffff] overflow-ellipsis overflow-hidden whitespace-nowrap">
               {board.title}
             </p>
           </div>
-          <div className="text-[0.6rem] overflow-ellipsis overflow-hidden whitespace-nowrap">
+          <div className="text-[0.6rem] text-[#ffffff]  overflow-ellipsis overflow-hidden whitespace-nowrap">
             테마 : {board.foodCategory}
           </div>
-          <div className="text-[0.6rem] overflow-ellipsis overflow-hidden whitespace-nowrap">
+          <div className="text-[0.6rem] text-[#ffffff]  overflow-ellipsis overflow-hidden whitespace-nowrap">
             현재인원 : {board.status} / {board.people}
           </div>
-          <div className="text-[0.6rem] overflow-ellipsis overflow-hidden whitespace-nowrap">
+          <div className="text-[0.6rem] text-[#ffffff]  overflow-ellipsis overflow-hidden whitespace-nowrap">
             약속시간 : {board.time}
           </div>
-          <button className="text-[0.6rem] mt-2 border w-[3.43rem] h-6 bg-[#54AB75] rounded-[0.625rem] text-[#ffffff]">
+          <button className="text-[0.6rem] text-[#ffffff]  mt-2 border w-[3.43rem] h-6 bg-[#54AB75] rounded-[0.625rem] text-[#ffffff]">
             <Link to={`/board/boardDetail/${board.boardId}`}>상세보기</Link>
           </button>
         </div>
         <img className="w-[6.5rem] h-[6.5rem]" src="img/boardexampleimg.svg" />
       </div>
-      <div className="w-3 h-3 absolute top-[97%] left-[4.3rem] bg-[#f1f1f1] transform rotate-45"></div>
+      <div className="w-3 h-3 absolute top-[97%] left-[4.3rem] bg-[#2a2a2a] transform rotate-45"></div>
     </div>
   );
 };
